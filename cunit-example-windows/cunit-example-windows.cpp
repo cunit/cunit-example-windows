99dd8679915db4c29cd8049de910b3a9b28bb081
Jonathan Glanz <jonathan.glanz@gmail.com>
2018-07-08 14:01:48 -0700

    CMake windows example works - JUST

---
+++ cunit-example-windows/cunit-example-windows.cpp
@@ -0,0 +1,16 @@
+﻿// cunit-example-windows.cpp : Defines the entry point for the application.
+//
+
+#include "cunit-example-windows.h"
+
+#include <test.pb.h>
+
+int main() {
+	std::cout << "Welcome to c-unit." << std::endl;
+	cunit::test::CUnitTestMessage msg;
+	msg.set_name("My name is c-unit");
+	std::cout << "Computer day what?? " << msg.name() << std::endl;
+	int c;
+	std::cin >> c;
+	return 0;
+}
